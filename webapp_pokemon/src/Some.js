1840b8eb28f76376ec72beab2df3d2ff8c407dda
Dan <Ockolok.3la@gmail.com>
2021-07-13 23:34:35 +0300

    Add display CP above Pokemon slots.

--- webapp_pokemon/src/Some.js
+++ webapp_pokemon/src/Some.js
@@ -1,11 +1,15 @@
-function EventOnPokeball() {
-  let NumberBall = document.querySelector(".PokemonBar button").getAttribute('ClassName')
+// function EventOnPokeball() {
+//   let NumberBall = document.querySelector(".PokemonBar button").getAttribute('ClassName')
+// }
+
+for (let elem of document.querySelectorAll(".PokemonBar div button")){
+  elem.addEventListener("focus", () => document.querySelector('div.menu').style.display="flex")
 }
 
-for (let elem of document.querySelectorAll("div.PokemonBar > button")){
-  elem.addEventListener("focus", () => document.querySelector('div.menu').style.display="unset")
-}
-
-document.querySelector("div.menu").addEventListener("blur", () => document.querySelector('div.menu').style.display="none")
-
-document.querySelector("div.menu form").addEventListener("submit", () => document.querySelector('div.PokemonBar').insertAdjacentElement())
+// let forma = document.querySelector('div.menu form');
+// forma.addEventListener('submit', function(ev) {
+//   if (document.querySelector("form input.PokemonID").value && (document.querySelector("form input.PokemonCP").value)) {
+//     document.querySelector('.PokemonBar button').insertAdjacentHTML('afterbegin','<img src="/bibla" alt="">')
+//     document.querySelector('.PokemonBar input').setAttribute(value,'document.querySelector("form input.PokemonCP").value')
+//   }
+// })
