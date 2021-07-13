8e8424de112abcf42aa9cc64a3fca2ce07e60201
archds <democlaus202@gmail.com>
2021-07-13 23:32:02 +0700

    fix resolvers name

--- server_pokemon/gql_api/resolvers/objects/pokemon.py
+++ server_pokemon/gql_api/resolvers/objects/pokemon.py
@@ -13,60 +13,64 @@ def resolve_name(obj: Pokemon, info):
 
 
 @gqt.pokemon.field('height')
-def resolve_name(obj: Pokemon, info):
+def resolve_height(obj: Pokemon, info):
     return obj.height
 
 
 @gqt.pokemon.field('weight')
-def resolve_name(obj: Pokemon, info):
+def resolve_weight(obj: Pokemon, info):
     return obj.weight
 
 
 @gqt.pokemon.field('maxCP')
-def resolve_name(obj: Pokemon, info):
+def resolve_max_cp(obj: Pokemon, info):
     return obj.max_cp
 
 
 @gqt.pokemon.field('familyName')
-def resolve_name(obj: Pokemon, info):
+def resolve_family_name(obj: Pokemon, info):
     return obj.family_name
 
 
 @gqt.pokemon.field('baseAttack')
-def resolve_name(obj: Pokemon, info):
+def resolve_base_attack(obj: Pokemon, info):
     return obj.base_attack
 
 
 @gqt.pokemon.field('baseDefense')
-def resolve_name(obj: Pokemon, info):
+def resolve_base_defense(obj: Pokemon, info):
     return obj.base_defense
 
 
 @gqt.pokemon.field('baseStamina')
-def resolve_name(obj: Pokemon, info):
+def resolve_base_stamina(obj: Pokemon, info):
     return obj.base_stamina
 
 
 @gqt.pokemon.field('primaryType')
-def resolve_name(obj: Pokemon, info):
+def resolve_primary_type(obj: Pokemon, info):
     return obj.primary_type
 
 
 @gqt.pokemon.field('secondaryType')
-def resolve_name(obj: Pokemon, info):
+def resolve_secondary_type(obj: Pokemon, info):
     return obj.secondary_type
 
 
 @gqt.pokemon.field('evolutionStage')
-def resolve_name(obj: Pokemon, info):
+def resolve_evolution_stage(obj: Pokemon, info):
     return obj.stage
 
 
 @gqt.pokemon.field('evolvesFrom')
-def resolve_name(obj: Pokemon, info):
+def resolve_evolves_from(obj: Pokemon, info):
     return obj.evolves_from
 
 
 @gqt.pokemon.field('evolveGender')
-def resolve_name(obj: Pokemon, info):
+def resolve_evolve_gender(obj: Pokemon, info):
     return obj.evolve_gender
+
+@gqt.pokemon.field('evolvesTo')
+def resolve_evolves_to(obj: Pokemon, info):
+    return obj.evolves_to.all()
