32a9740b57aacd936e94edc50679a8731358c632
archds <democlaus202@gmail.com>
2021-07-13 21:40:42 +0700

    fix gql

--- server_pokemon/gql_api/resolvers/objects/pokemon.py
+++ server_pokemon/gql_api/resolvers/objects/pokemon.py
@@ -1,29 +1,36 @@
 import gql_api.gql_api_types as gqt
 from pokemon_base.models import Pokemon
 
+
 @gqt.pokemon.field('id')
 def resolve_id(obj: Pokemon, info):
     return obj.id
+
 
 @gqt.pokemon.field('name')
 def resolve_name(obj: Pokemon, info):
     return obj.name
 
+
 @gqt.pokemon.field('height')
 def resolve_name(obj: Pokemon, info):
     return obj.height
+
 
 @gqt.pokemon.field('weight')
 def resolve_name(obj: Pokemon, info):
     return obj.weight
 
+
 @gqt.pokemon.field('maxCP')
 def resolve_name(obj: Pokemon, info):
     return obj.max_cp
 
+
 @gqt.pokemon.field('familyName')
 def resolve_name(obj: Pokemon, info):
     return obj.family_name
+
 
 @gqt.pokemon.field('baseAttack')
 def resolve_name(obj: Pokemon, info):
@@ -34,26 +41,32 @@ def resolve_name(obj: Pokemon, info):
 def resolve_name(obj: Pokemon, info):
     return obj.base_defense
 
+
 @gqt.pokemon.field('baseStamina')
 def resolve_name(obj: Pokemon, info):
     return obj.base_stamina
+
 
 @gqt.pokemon.field('primaryType')
 def resolve_name(obj: Pokemon, info):
     return obj.primary_type
 
+
 @gqt.pokemon.field('secondaryType')
 def resolve_name(obj: Pokemon, info):
     return obj.secondary_type
+
 
 @gqt.pokemon.field('evolutionStage')
 def resolve_name(obj: Pokemon, info):
     return obj.stage
 
-@gqt.pokemon.field('infancy')
-def resolve_name(obj: Pokemon, info):
-    return obj.infancy
 
-@gqt.pokemon.field('infancyGender')
+@gqt.pokemon.field('evolvesFrom')
 def resolve_name(obj: Pokemon, info):
-    return obj.infancy_gender
+    return obj.evolves_from
+
+
+@gqt.pokemon.field('evolveGender')
+def resolve_name(obj: Pokemon, info):
+    return obj.evolve_gender
