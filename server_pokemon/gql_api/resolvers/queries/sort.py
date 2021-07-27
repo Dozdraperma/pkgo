a4f413e909b7feba3c3df532183daf8c247c8155
archds <democlaus202@gmail.com>
2021-07-27 10:38:16 +0700

    sort refactor

--- server_pokemon/gql_api/resolvers/queries/sort.py
+++ server_pokemon/gql_api/resolvers/queries/sort.py
@@ -1,10 +1,8 @@
 from typing import Dict
 
-from ariadne import convert_kwargs_to_snake_case
 from django.db.models import QuerySet
 
 
 def sort_pokemon(sort_info: Dict, query: QuerySet):
-    for sort_type in sort_info['sort_by']:
-        query = query.order_by(sort_type)
+    query = query.order_by(*sort_info['sort_by'])
     return query
