b8d6feef55fbe70f8dbe36b6ee6baaedbeadb8b2
Dan <Ockolok.3la@gmail.com>
2021-08-29 14:40:33 +0300

    Аполло

--- vueapp_pokemon/src/views/PokeMenu.vue
+++ vueapp_pokemon/src/views/PokeMenu.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="contan">
-  <div class="PokemonMenu"
-       v-bind:style="{ visibility: isVisibl } "
-  >
-    <form @submit.prevent="onSubmit">
-      <input type="text" placeholder="Имя" v-model="name">
-      <div class="wrappy">
-        <div class="search" v-show="vis">
-          <button  @click="ekanS (item.name)" v-for="item in listPokemon" :key="item">{{ item.name }}</button>
+    <div class="PokemonMenu"
+         v-bind:style="{ visibility: isVisibl } "
+    >
+      <form @submit.prevent="onSubmit">
+        <input type="text" placeholder="Имя" v-model="name" >
+        <div class="wrappy">
+          <div class="search" v-show="vis">
+            <button  @click="ekanS (item.name)" v-for="item in listPokemon" :key="item">{{ item.name }}</button>
+          </div>
         </div>
-      </div>
-      <input type="text" placeholder="СР" maxlength="4" v-model="sp">
-      <br>
-      <button type="submit">ДОБАВИТЬ</button>
-    </form>
-  </div>
+        <input type="text" placeholder="СР" maxlength="4" v-model="sp">
+        <br>
+        <button type="submit">ДОБАВИТЬ</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -27,18 +27,16 @@ export default {
       this.name = item
     },
     onSubmit () {
-      if (this.sp > 9) {
-        this.$emit('onsubmit', {
-          name: this.name,
-          sp: this.sp
-        })
-      }
+      this.$emit('onsubmit', {
+        name: this.name,
+        sp: this.sp
+      })
     }
   },
   watch: {
     name: function (name) {
       const data = { query: `{searchPokemons(search: {name:"${name}"includeEvolutions:false}) {name}}` }
-      if (name) {
+      if (name !== '') {
         this.vis = false
         this.listPokemon = []
         fetch('http://127.0.0.1:8000/api', {
@@ -50,7 +48,7 @@ export default {
           body: JSON.stringify(data)
         }).then((response) => response.json())
           .then((json) => {
-            if (json.data.searchPokemons !== null && !(json.data.searchPokemons.length === 1 && json.data.searchPokemons[0].name.toLowerCase() === name.toLowerCase())) {
+            if (!(json.data.searchPokemons.length === 1 && json.data.searchPokemons[0].name.toLowerCase() === name.toLowerCase())) {
               for (let i = 0; i <= json.data.searchPokemons.length - 1; i++) {
                 this.listPokemon.push(json.data.searchPokemons[i])
               }
@@ -84,22 +82,21 @@ export default {
   z-index: 2;
   background: #ffffff;
   position: absolute;
+  margin: 0 20%;
   border-radius: 10px;
   padding: 5px;
   box-shadow: 0 0 5px 3px rgba(123, 161, 160, 0.3);
   width: 100%;
-
-form input {
-  font-family: "Montserrat", "Helvetica", "Roboto", sans-serif;
-  border-radius: 20px;
-  border: none;
-  background: #d7ecef;
-  padding: 7px;
-  text-align: center;
-  margin: 5px 0;
-  width: 70%;
-
-}
+  form input {
+    font-family: "Montserrat", "Helvetica", "Roboto", sans-serif;
+    border-radius: 20px;
+    border: none;
+    background: #d7ecef;
+    padding: 7px;
+    text-align: center;
+    margin: 5px 0;
+    width: 70%;
+  }
   form {
     .wrappy {
       display: flex;
@@ -117,15 +114,15 @@ form input {
       }
     }
   }
-button {
-  font-family: 'Encode Sans SC', "Helvetica", "Roboto", sans-serif;
-  padding: 4px 9px;
-  margin-top: 5px;
-  font-weight: 600;
-  background-color: #38a6eb;
-  color: #2c3e50;
-  border: 0;
-  border-radius: 15px;
-}
+  button {
+    font-family: 'Encode Sans SC', "Helvetica", "Roboto", sans-serif;
+    padding: 4px 9px;
+    margin-top: 5px;
+    font-weight: 600;
+    background-color: #38a6eb;
+    color: #2c3e50;
+    border: 0;
+    border-radius: 15px;
+  }
 }
 </style>
--- vueapp_pokemon/src/views/PokemonSlot.vue
+++ vueapp_pokemon/src/views/PokemonSlot.vue
@@ -1,19 +1,15 @@
 <template>
-<div class="PokemonSlot" >
-  <div class="wRap">
+  <div class="PokemonSlot" >
     <div class="remove"
          v-bind:style="{visibility: isVisibleinfo}"
-         @click="onRemove">
-    </div>
-  </div>
+         @click="onRemove"></div>
     <p v-bind:style="{visibility: isVisibleinfo}">CP <span>{{ Pokemon.cp }}</span></p>
     <button v-on:click="openMenu">
-      <img v-if="Pokemon.id" :src="require(`../../src/views/media/Pokemon/pokemon_icon_${('1000' + Pokemon.id).slice(-3)}_00.png`)" alt="">
     </button>
     <p id="name" v-bind:style="{visibility: isVisibleinfo}">|{{ Pokemon.name }}|</p>
-  <PokeMenu v-bind:isVisibl="isVisible"
-            v-on:onsubmit="onSubmit"
-  />
+    <PokeMenu v-bind:isVisibl="isVisible"
+              v-on:onsubmit="onSubmit"
+    />
   </div>
 </template>
 
@@ -27,15 +23,7 @@ export default {
       this.isVisible === 'hidden' ? this.isVisible = 'visible' : this.isVisible = 'hidden'
     },
     onSubmit (ab) {
-      fetch('http://127.0.0.1:8000/api', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ query: `{searchPokemons(search: {name:"${ab.name}"includeEvolutions:false}) {id}}` })
-      }).then((response) => response.json())
-        .then((json) => { this.Pokemon.id = json.data.searchPokemons[0].id })
+      this.Pokemon.id = Date.now()
       this.Pokemon.name = ab.name
       this.Pokemon.cp = ab.sp
       this.isVisibleinfo = 'visible'
@@ -62,15 +50,10 @@ export default {
   font-family: "Montserrat", "Helvetica", "Roboto", sans-serif;
   padding: 5px 5px;
   position: relative;
-  img{
-    max-width: 100%;
-    max-height: 50%;
-  }
   p {
     margin: 7px;
     font-size: 10px;
     font-weight: bold;
-
     span {
       font-size: 20px;
     }
@@ -80,40 +63,33 @@ export default {
     font-size: 15px;
     font-weight: bold;
   }
-
   button {
     border-radius: 10px;
     box-shadow: 0 0 5px 3px rgba(123, 161, 160, 0.2);
-    width: 70px;
-    height: 70px;
+    padding: 30px;
     border: 2px solid #bad7db;
     background: linear-gradient(90deg, #ffffff, #f7fdf3);
   }
-
   button:hover {
     box-shadow: 0 0 5px 7px rgba(123, 161, 160, 0.8);
     transition-duration: 200ms;
   }
-  .wRap {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    .remove {
-      opacity: 0;
-      z-index: 4;
-      background-image: url('media/free-icon-cross-black-circular-button-54473.svg');
-      background-repeat: no-repeat;
-      background-size: 50px 50px;
-      background-position: center;
-      position: absolute;
-      top: 37px;
-      width: 70px;
-      height: 70px;
-    }
-    .remove:hover {
-      opacity: 0.5;
-      transition-duration: 500ms;
-    }
+  .remove {
+    opacity: 0;
+    z-index: 4;
+    background-image: url('media/free-icon-cross-black-circular-button-54473.svg');
+    background-repeat: no-repeat;
+    background-size: 50px 50px;
+    background-position: center;
+    position: absolute;
+    top: 39px;
+    left: 15px;
+    width: 70px;
+    height: 70px;
+  }
+  .remove:hover {
+    opacity: 0.5;
+    transition-duration: 500ms;
   }
 }
 </style>
